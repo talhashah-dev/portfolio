0d9c1f4fe933218509d7dcd57f29985315f8023d
Syed Talha <syedt.in00@gmail.com>
2024-06-18 02:07:14 -0700

    modified Contact.jsx
    added content to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,17 @@ import React from "react";
 function Conatct(){
   return (
     <>
-      <div></div>
+      <div className="text-center flex flex-col gap-5">
+        <div class="text-5xl mb-10">Contact Me ☎️</div>
+        <p class="text-gray-400 text-base">
+          DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.
+        </p>
+        <a href="tel:+923349009827" class="text-gray-400 text-2xl hover:text-white">+92-3349009827</a>
+        <a href="mailto:syedt.in00@gmail.com" class="text-gray-400 text-2xl hover:text-white">syedt.in00@gmail.com</a>
+        <div className="flex gap-5">
+          
+        </div>
+      </div>
     </>
   )
 }
