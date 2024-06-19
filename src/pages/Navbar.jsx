f74e8e2194b07dac3b492ade1cee27df18a7f854
Syed Talha <syedt.in00@gmail.com>
2024-06-19 13:35:17 -0700

    modified navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -7,7 +7,9 @@ function Navbar() {
     <>
     <div className="container mx-auto flex justify-between items-center pt-8 mb-12">
         <div className="title">
-          <h1 className="text-4xl font-bold mb-2">Hello, I'm <span className="text-gray-300">Talha</span> 👋</h1>
+          <Link to="/">
+            <h1 className="text-2xl italic mb-2 text-gray-400 hover:text-white"> &lt;syedtalha /&gt;</h1>
+          </Link>
         </div>
         <nav className="flex gap-10">
             <Link to="/" className="text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all">About</Link>
