a9de82395fae7d3bd19a1c2ee5c29d0584c0760f
Syed Talha <syedt.in00@gmail.com>
2024-06-18 23:54:40 -0700

    fixed html to jsx classname issue

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,12 +5,12 @@ function Conatct(){
   return (
     <>
       <div className="text-center flex flex-col gap-5">
-        <div class="text-5xl mb-10">Contact Me ☎️</div>
-        <p class="text-gray-400 text-base">
+        <div className="text-5xl mb-10">Contact Me ☎️</div>
+        <p className="text-gray-400 text-base">
           DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.
         </p>
-        <a href="tel:+923349009827" class="text-gray-400 text-2xl hover:text-white">+92-3349009827</a>
-        <a href="mailto:syedt.in00@gmail.com" class="text-gray-400 text-2xl hover:text-white">syedt.in00@gmail.com</a>
+        <a href="tel:+923349009827" className="text-gray-400 text-2xl hover:text-white">+92-3349009827</a>
+        <a href="mailto:syedt.in00@gmail.com" className="text-gray-400 text-2xl hover:text-white">syedt.in00@gmail.com</a>
         <div className="flex justify-center gap-5 mt-10">
           <a href="https://github.com/talhashah-dev" target="_black" className="text-4xl cursor-pointer " >
             <FaGithub />
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,10 +6,10 @@ function Projects() {
   return (
     <div className="flex flex-wrap justify-around gap-y-10 pb-5">
 
-      <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:scale-105 shadow-2xl hover:shadow-indigo-500/50 transition-all">
-        <img class="w-full" src={Weather_App} alt="Sunset in the mountains" />
-        <div class="px-4">
-          <div class="flex items-center gap-5">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:scale-105 shadow-2xl hover:shadow-indigo-500/50 transition-all">
+        <img className="w-full" src={Weather_App} alt="Sunset in the mountains" />
+        <div className="px-4">
+          <div className="flex items-center gap-5">
             <h1 className="font-bold text-xl mb-1 mt-1 text-black">
               Weather App 
             </h1>
@@ -20,21 +20,21 @@ function Projects() {
               <FaExternalLinkAlt /> 
             </a>
             </div>
-          <p class="text-gray-700 text-base">
+          <p className="text-gray-700 text-base">
           A Weather App built on React.js using Openweathermap API
           </p>
         </div>
-        <div class="px-6 pt-4 pb-2">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">react</span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">api</span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">css</span>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">react</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">api</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">css</span>
         </div>
       </div>
 
-      <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:scale-105 shadow-2xl hover:shadow-indigo-500/50 transition-all">
-        <img class="w-full" src={Blog_App} alt="Sunset in the mountains" />
-        <div class="px-4">
-          <div class="flex items-center gap-5">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:scale-105 shadow-2xl hover:shadow-indigo-500/50 transition-all">
+        <img className="w-full" src={Blog_App} alt="Sunset in the mountains" />
+        <div className="px-4">
+          <div className="flex items-center gap-5">
             <h1 className="font-bold text-xl mb-1 mt-1 text-black">
               Blog App
             </h1>
@@ -45,15 +45,15 @@ function Projects() {
               <FaExternalLinkAlt /> 
             </a>
             </div>
-            <p class="text-gray-700 text-base">
+            <p className="text-gray-700 text-base">
               Write your thoughts and publish them online on a secure and easy-to-use platform.
             </p>
         </div>
-        <div class="px-6 pt-4 pb-2">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">html</span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">css</span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">javascript</span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">firebase</span>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">html</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">css</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">javascript</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">firebase</span>
         </div>
       </div>
 
