c31880e8b52db43141520252280b04de37d01103
Syed Talha <syedt.in00@gmail.com>
2024-06-20 03:50:06 -0700

    modified navbar
    added links classname toggle for active and deactive

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css"
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [active, setActive] = useState(0);
+  const handleLinkClick = (index) => {
+    setActive(index);
+  };
   return (
     <>
     <div className="container mx-auto flex justify-between items-center pt-8 mb-12">
         <div className="title">
           <Link to="/">
-            <h1 className="text-3xl italic mb-2 text-gray-400 hover:text-white signature">&lt;Syed Talha /&gt;</h1>
+            <h1 className={`text-3xl italic mb-2 text-gray-400 hover:text-white signature`} onClick={() => handleLinkClick(0)}>&lt;Syed Talha /&gt;</h1>
           </Link>
         </div>
         <nav className="flex gap-10">
-            <Link to="/" className="text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all">About</Link>
-            <Link to="/skills" className="text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all">Skills</Link>
-            <Link to="/projects" className="text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all">Projects</Link>
-            <Link to="/contact" className="text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all">Contact</Link>
+            <Link to="/" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 0 ? "active" : "deactive"}`} onClick={() => handleLinkClick(0)}>About</Link>
+            <Link to="/skills" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 1 ? "active" : "deactive"}`} onClick={() => handleLinkClick(1)}>Skills</Link>
+            <Link to="/projects" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 2 ? "active" : "deactive"}`} onClick={() => handleLinkClick(2)}>Projects</Link>
+            <Link to="/contact" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 3 ? "active" : "deactive"}`} onClick={() => handleLinkClick(3)}>Contact</Link>
         </nav>
     </div>
     </>
