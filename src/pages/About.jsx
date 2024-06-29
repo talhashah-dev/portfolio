74142fc53689bab69217d1385d0c237ea785a5a6
Syed Talha <syedt.in00@gmail.com>
2024-06-29 11:28:29 -0400

    modified about page text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,11 +8,13 @@ function About() {
         <div className="w-1/2 max-[426px]:w-full text-center">
           <h1 className="text-4xl font-bold mb-5">Hi, I'm <span className="text-gray-300">Talha</span> 👋</h1>
           <p className="text-[#7e9199] mb-5">
-            I am a Full Stack Web Developer from Mauritania. I have a passion
-            for creating seamless,beautiful and creative websites, I have
+            I am a Frontend Developer from Karachi. I have a passion
+            for creating seamless, beautiful and creative websites, I have
             experience with various programming languages and specifically web
-            technologies . Life is a continuous learning process, so work makes
-            me more rigorous . I am currently available to hire .
+            technologies. <br />
+            Life is a continuous learning process, so work makes
+            me more rigorous. <br />
+            I am currently available to hire .
           </p>
           <a href="mailto:syedt.in00@gmail.com" className="border border-white p-2 hover:border-[#00B2DF] hover:text-[#00B2DF] transition-all inline-block w-1/2 max-[426px]:w-full">Get in touch</a>
         </div>
