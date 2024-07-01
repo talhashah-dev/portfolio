1d48fbadbb12d2b3908527727b85f3b73c5e6e02
Syed Talha <syedt.in00@gmail.com>
2024-07-01 12:57:29 -0400

    fix:Project page height

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="bg-[#171C28] max-[426px]:px-4 max-[787px]:px-4 text-white px-44 min-h-screen">
+    <div className="bg-[#171C28] max-[426px]:px-4 max-[787px]:px-4 text-white px-44 min-h-screen pb-10">
       <Navbar />
       <Routes>
         <Route path="/" element={<About />} />
