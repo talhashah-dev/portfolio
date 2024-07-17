3ae33b0f7398da9cd6c961c84a86bbbced262726
Syed <syedt.in00@gmail.com>
2024-07-17 15:51:13 +0500

    feat: scroll to top function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { About, Skills, Projects, Contact } from "./pages";
 import { Navbar, Loader, Footer } from "./components/index.js";
+import { GoArrowUp } from "react-icons/go";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
 
   const handleDataFromChild = (data) => {
     setMenuOpen(data);
@@ -18,6 +20,20 @@ function App() {
     }, 1500);
   }, []);
 
+  const toggleVisible = () => { 
+    const scrolled = document.documentElement.scrollTop; 
+    scrolled > 300 ? setVisible(true) : setVisible(false);
+  }; 
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+
+  window.addEventListener("scroll", toggleVisible)
+  
 
   return (
     <div className="bg-[#171C28] max-[426px]:px-4 max-[787px]:px-4 text-white px-44 min-h-screen pb-10">
@@ -27,6 +43,7 @@ function App() {
       ) : (
         !menuOpen && (
           <>
+          <button title="GO TO TOP" className={`rounded-lg py-4 px-2 text-2xl text-purple-500 hover:text-white bg-white hover:bg-purple-500 fixed right-10 bottom-10 ${visible ? "block" : "hidden"}`} onClick={scrollToTop}><GoArrowUp /></button>
           <About />
           <Skills />
           <Projects />
