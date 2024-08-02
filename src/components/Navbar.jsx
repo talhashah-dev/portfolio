adbcefb77cbc7ab17da8ee4ae9baa6f95624c4f2
Syed Talha <syedt.in00@gmail.com>
2024-08-02 22:29:20 +0500

    refactor: navbar stracture modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { IoMdMenu, IoMdClose } from "react-icons/io";
 
 
 function Navbar({sendDataToParent}) {
-  const [active, setActive] = useState(0);
+  // const [active, setActive] = useState(0);
   const [menuOpen, setMenuOpen] = useState(false);
   
   const handleLinkClick = (index) => {
-    setActive(index);
+    // setActive(index);
     setMenuOpen(false);
     sendDataToParent(false);
   };
@@ -31,27 +31,44 @@ function Navbar({sendDataToParent}) {
           <button className="text-2xl hidden max-md:block" onClick={() => handleMenu()}>{menuOpen ? <IoMdClose /> : <IoMdMenu />}</button>
         </div>
         <nav className={`flex gap-10 max-md:flex-col max-md:mt-12 ${menuOpen ? "max-md:flex" : "max-md:hidden"} `}>
-            <a href="#about" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 0 ? "active" : "deactive"}`} onClick={() => handleLinkClick(0)}>
-              About
-            </a>
-            <a href="#skills" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 1 ? "active" : "deactive"}`} onClick={() => handleLinkClick(1)}>
-              Skills
-            </a>
-            <a href="#projects" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 2 ? "active" : "deactive"}`} onClick={() => handleLinkClick(2)}>
-              Projects
-            </a>
-            <a href="#certificates" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 3 ? "active" : "deactive"}`} onClick={() => handleLinkClick(3)}>
-              Certificates
-            </a>
-            <a href="#contact" className={`text-[#7e9199] hover:text-white pb-1 hover:border-b text-lg transition-all ${active === 4 ? "active" : "deactive"}`} onClick={() => handleLinkClick(4)}>
-              Contact
-            </a>
+          <ul className="flex gap-10 max-md:flex-col max-md:w-full">
+            <li className="max-md:w-full">
+              <a href="#about" className={`inline-block w-full text-[#7e9199] hover:text-white max-md:hover:border-none max-md:active:bg-purple-500 max-md:active:text-white max-md:px-2 hover:border-b pb-1 text-lg`} onClick={() => handleLinkClick()}>
+                About
+              </a>
+            </li>
+
+            <li>
+              <a href="#skills" className={`inline-block w-full text-[#7e9199] hover:text-white max-md:hover:border-none max-md:active:bg-purple-500 max-md:active:text-white max-md:px-2 hover:border-b pb-1 text-lg`} onClick={() => handleLinkClick()}>
+                Skills
+              </a>
+            </li>
+
+            <li>
+              <a href="#projects" className={`inline-block w-full text-[#7e9199] hover:text-white max-md:hover:border-none max-md:active:bg-purple-500 max-md:active:text-white max-md:px-2 hover:border-b pb-1 text-lg`} onClick={() => handleLinkClick()}>
+                Projects
+              </a>
+            </li>
+            
+            <li>
+              <a href="#certificates" className={`inline-block w-full text-[#7e9199] hover:text-white max-md:hover:border-none max-md:active:bg-purple-500 max-md:active:text-white max-md:px-2 hover:border-b pb-1 text-lg`} onClick={() => handleLinkClick()}>
+                Certificates
+              </a>
+            </li>
+            
+            <li>
+              <a href="#contact" className={`inline-block w-full text-[#7e9199] hover:text-white max-md:hover:border-none max-md:active:bg-purple-500 max-md:active:text-white max-md:px-2 hover:border-b pb-1 text-lg`} onClick={() => handleLinkClick()}>
+                Contact
+              </a>
+            </li>
+          </ul>
+          
           <ul className={`hidden justify-around items-center mt-28 max-md:flex`}>
             <li><a href="https://github.com/talhashah-dev" className="text-4xl" target="_blank" rel="noreferrer"><FaGithub /></a></li>
             <li><a href="https://www.linkedin.com/in/talhashah-dev/" className="text-4xl" target="_blank" rel="noreferrer"><FaLinkedin /></a></li>
             <li><a href="https://stackoverflow.com/users/23691689/talha-shah" className="text-4xl" target="_blank" rel="noreferrer"><FaStackOverflow /></a></li>
           </ul>
-        </nav>  
+      </nav>  
         
     </div>
     </>
